b27b3c44be1f746342b145149513409dabb97edb
Muzammil khan <khanmuzammil549@gmail.com>
2022-08-10 23:21:52 +0530

    Fix: added route for homepage

--- src/components/Landing-Page/Content.jsx
+++ src/components/Landing-Page/Content.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import "../Landing-Page/content.css"
  
 const Content = () => {
@@ -7,7 +8,7 @@ const Content = () => {
         <div className='content-title'> This is your note taking application. </div>
         <div className='content-description'>Here you can easily add notes related to your daily tasks in an organized manner. </div>
         <div className='content-description'> All you need is to create an account.</div>
-        <button className='content-btn'>Get started</button>
+        <button className='content-btn'><Link className='link' to="/home"> Get started</Link></button>
     </div>
   )
 }
