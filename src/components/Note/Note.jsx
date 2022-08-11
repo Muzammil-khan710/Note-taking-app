83e7c93a73233224315a63a105e4f8a21daede38
Muzammil khan <khanmuzammil549@gmail.com>
2022-08-11 13:22:28 +0530

    Feat: added rich text editor

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JoditEditor from "jodit-react"
+
+const Note = () => {
+  return (
+    <div>
+        <JoditEditor />
+    </div>
+  )
+}
+
+export default Note
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
+import Note from './../components/Note/Note';
 
 const HomePage = () => {
   return (
     <div>
         <Navbar/>
+        <Note/>
     </div>
   )
 }
