a8839f87da9eaac0acfcecdceaf3118eeba3475f
Muzammil khan <khanmuzammil549@gmail.com>
2022-08-18 11:21:00 +0530

    Style : changed color on hovering the links at landing page

--- src/components/Landing-Page/Content.jsx
+++ src/components/Landing-Page/Content.jsx
@@ -7,7 +7,7 @@ const Content = () => {
     <div className='content-container'>
         <div className='content-title'> This is your note taking application. </div>
         <div className='content-description'>Here you can easily add notes related to your daily tasks in an organized manner. </div>
-        <div className='content-description'> All you need is to<Link className='link bb-2' to="/login"> Sign in</Link> or <Link className='link bb-2' to="/singup"> Create an account  </Link>.</div>
+        <div className='content-description'> All you need is to<Link className='link bb-2 l-hvr' to="/login"> Sign in</Link> or <Link className='link bb-2 l-hvr' to="/signup"> Create an account  </Link>.</div>
         <button className='content-btn btn-hover'><Link className='link' to="/home"> Get started</Link></button>
     </div>
   )
