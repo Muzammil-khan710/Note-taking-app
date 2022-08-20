466cfc9e5fe4f9177e9c6ebf2b4ffb27c1e55618
Muzammil khan <khanmuzammil549@gmail.com>
2022-08-20 14:56:45 +0530

    Fixed : auth and private routes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,8 +6,8 @@ const Sidebar = () => {
   return (
     <div className='side-container'>
         <NavLink className='side-link' to='/home'>Home</NavLink>
-        <NavLink className='side-link'>Archive</NavLink>
-        <NavLink className='side-link'>Trash</NavLink>
+        <NavLink className='side-link' to='/archive'>Archive</NavLink>
+        <NavLink className='side-link' to='/trash'>Trash</NavLink>
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import { Login } from "./Login/Login";
 import { Signup } from "./Signup/Signup";
 import { Content } from "./Landing-Page/Content"
 import { Note } from "./Note/Note";
+import { Authroute } from './Authroute';
+import { Privateroute } from './Privateroute';
 
-export { Navbar, Login, Signup, Content, Note }
+export { Navbar, Login, Signup, Content, Note, Authroute, Privateroute }
